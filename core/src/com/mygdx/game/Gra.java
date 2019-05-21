96308c7abad2890985d2a54d004d2b8a060b0d63
Marcin795 <38189523+Marcin795@users.noreply.github.com>
2019-05-21 21:21:08 +0200

    test: added golden rect

--- core/src/com/mygdx/game/Gra.java
+++ core/src/com/mygdx/game/Gra.java
@@ -2,17 +2,21 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Gra extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	ShapeRenderer renderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		renderer = new ShapeRenderer();
 		img = new Texture("badlogic.jpg");
 	}
 
@@ -21,8 +25,14 @@ public class Gra extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+
 //		batch.draw(img, 0, 0);
 		batch.end();
+
+		renderer.begin();
+		renderer.setColor(Color.GOLD);
+		renderer.rect(0, 0, 100, 100);
+		renderer.end();
 	}
 	
 	@Override
