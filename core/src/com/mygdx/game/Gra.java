67ece7294a1ce9d9d41b7151d6b61c2a677ae67f
pamiiixx <PsiaczeK59>
2019-05-22 15:53:16 +0200

    Merge branch 'master' of C:\Users\Paulina\IdeaProjects\Gra with conflicts.

--- core/src/com/mygdx/game/Gra.java
+++ core/src/com/mygdx/game/Gra.java
@@ -2,36 +2,56 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Gra extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	ShapeRenderer renderer;
-	
+	Camera camera;
+	Viewport viewport;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		renderer = new ShapeRenderer();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(400, 400, camera);
 	}
 
-	@Override
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
 	public void render () {
+	    viewport.apply();
+	    batch.setProjectionMatrix(viewport.getCamera().combined);
+        renderer.setProjectionMatrix(viewport.getCamera().combined);
+
 		Gdx.gl.glClearColor(1, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 
 //		batch.draw(img, 0, 0);
 		batch.end();
-		renderer.begin(ShapeRenderer.ShapeType.Filled);
+//		renderer.begin(ShapeRenderer.ShapeType.Filled);
+        renderer.setAutoShapeType(true);
+		renderer.begin();
 		renderer.setColor(Color.GOLD);
-		renderer.rect(0, 0, 100, 100);
-		renderer.end();
+        renderer.set(ShapeRenderer.ShapeType.Filled);
+		renderer.rect(-200, -200, 200, 200);
+        renderer.setColor(Color.RED);
+//        renderer.set(ShapeRenderer.ShapeType.Filled);
+        renderer.rect(0, 0, 200, 200);
+
+        renderer.end();
 	}
 	
 	@Override
