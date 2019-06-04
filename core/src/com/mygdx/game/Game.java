4faca00378dad44188aeacadb27d86d9abe3eb50
Marcin795 <38189523+Marcin795@users.noreply.github.com>
2019-06-04 20:39:52 +0200

    add test level v2

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -20,6 +21,8 @@ public class Game extends InputAdapter implements Screen {
     protected Viewport viewport;
     Camera camera;
     ShapeRenderer renderer;
+    Balls balls;
+    BallPaths ballPaths;
 
 
     public Game(Gra gra){
@@ -30,29 +33,44 @@ public class Game extends InputAdapter implements Screen {
     public void show() {
         camera = new OrthographicCamera();
         batch = new SpriteBatch();
+        renderer = new ShapeRenderer();
         viewport = new ExtendViewport(Constants.minWorldWidth,Constants.minWorldHeight,camera);
         Gdx.input.setInputProcessor(this);
+        balls = new Balls(viewport);
+        ballPaths = new BallPaths(viewport);
     }
 
     @Override
     public void render(float delta) {
         viewport.apply();
         batch.setProjectionMatrix(viewport.getCamera().combined);
-
+        renderer.setProjectionMatrix(viewport.getCamera().combined);
+        renderer.setAutoShapeType(true);
         Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //renderer.setProjectionMatrix(viewport.getCamera().combined);
+        balls.update(delta);
 
+        renderer.begin();
         batch.begin();
 
+        ballPaths.render(renderer);
+
+        balls.render(renderer);
+
+
+        renderer.end();
         batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
         viewport.update(width,height,true);
+        Gdx.app.log("Game", viewport.getScreenWidth()+" resize");
         camera.update();
+
+        balls.init();
+        ballPaths.init();
     }
 
     @Override
@@ -68,7 +86,6 @@ public class Game extends InputAdapter implements Screen {
     @Override
     public void hide() {
         batch.dispose();
-        //font.dispose();
         renderer.dispose();
     }
 
